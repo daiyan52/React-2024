032d52689e4bdd93707b5ec1bce0cd9dfeda74f9
Daiyan Alam <daiyanalam@Daiyans-MacBook-Air.local>
2023-12-26 02:36:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import ProductTab from './Components/ProductTab';
+import 'bootstrap/dist/css/bootstrap.css';
+function App() {
+  return (
+    <ProductTab/>
+   )
+}
+
+export default App
---
+++ src/Components/Product.jsx
@@ -0,0 +1,11 @@
+import '../CSS/Product.css';
+function Product(){
+    return(
+        <div className="Product">
+            <h2 className='text-danger'>Product Title</h2>
+            <h3 className="text-muted">Product Description</h3>
+            {/* <button className="btn btn-primary">Read Details</button> */}
+        </div>
+    )
+}
+export default Product;
