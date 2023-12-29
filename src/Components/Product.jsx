6d476f9abb45b86798e552536fb5279d2c3b57e6
Daiyan Alam <daiyanalam@Daiyans-MacBook-Air.local>
2023-12-30 01:34:11 +0530

    670

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,9 +1,15 @@
 import '../CSS/Product.css';
-function Product(){
+function Product({title,price,features}){
+    let list = features.map((feature) => 
+    <li key={feature.id}>{feature}</li>)
+    console.log(list);
     return(
         <div className="Product">
-            <h2 className='text-danger'>Product Title</h2>
-            <h3 className="text-muted">Product Description</h3>
+            <div>
+                <h2 className='text-danger'>{title}</h2>
+                <h3 className="text-muted">Price: {price}</h3>
+                <p>{list}</p>
+             </div>
             {/* <button className="btn btn-primary">Read Details</button> */}
         </div>
     )
--- src/Components/ProductTab.jsx
+++ src/Components/ProductTab.jsx
@@ -1,11 +1,15 @@
 import Product from "./Product";
 
 function ProductTab(){
+    // let option1 = ["8GB RAM","i5","Mi"];
+    let options = ["8GB RAM","i5","Mi"];
+
+    // let option2 = {a:"8GB RAM", b:"i5",c:"Mi"};
     return(
         <>
-        <Product/>
-        <Product/>
-        <Product/>
+        <Product title="iphone" price={89000} features={options}/>
+        <Product title="Macbook" price={6999} />
+        <Product title="Pen Tablet" price={2199}/>
         </>
     )
 }
